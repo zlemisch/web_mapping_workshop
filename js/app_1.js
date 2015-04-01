8effbe86cebfa53ca20e6e41ab6b75f7fb013924
zlemisch <zlemisch@users.noreply.github.com>
2015-03-31 20:46:48 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -4,7 +4,6 @@
 var mapId = 'zlemisch.bec3e5a3'; //<- this references the ugly green map that I made
 var token = 'pk.eyJ1IjoiemxlbWlzY2giLCJhIjoiT1BRSzVqayJ9.cg-8D1PD29JwmtEa4T6AUA'; //<- this is my token, use yours.
 
-
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
 var map = L.mapbox.map('map', mapId);
