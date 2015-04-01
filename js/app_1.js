fe81d2cef6fbcb8752d642af38394ee81cbd39c7
zlemisch <zlemisch@users.noreply.github.com>
2015-03-31 20:01:41 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////
 // Enter your mapbox map id here to reference it for the base layer
 
-var mapId = 'will-breitkreutz.k6fj4l3f'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var mapId = 'zlemisch.bec3e5a3'; //<- this references the ugly green map that I made
+var token = 'pk.eyJ1IjoiemxlbWlzY2giLCJhIjoiT1BRSzVqayJ9.cg-8D1PD29JwmtEa4T6AUA'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
