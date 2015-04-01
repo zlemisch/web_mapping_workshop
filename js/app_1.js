f7f97e56ee747e206fe3a152e213c73e2c222158
zlemisch <zlemisch@users.noreply.github.com>
2015-03-31 20:31:36 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -11,3 +11,17 @@ var map = L.mapbox.map('map', mapId);
 
 //Set the view of the map to the whole US
 map.setView([39, -96], 4);
+
+var dataFileToAdd = 'data/parks.geojason';
+
+var featurelayer = L.mapbox.featureLayer().loadURL(dataFileToAdd).addTo(map);
+
+featureLayer.on('ready',function(){
+  this.setStyle({
+    'color':'#ec008c',
+    'fillColor':'#ec088c',
+    'weight':4,
+    'opacity':.7
+  });
+  map.fitBounds(featurelLayer.getBounds());
+})
